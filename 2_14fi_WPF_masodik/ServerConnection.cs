e5e1ba867f25942fadc184268369ea1704c4d4f9
zsar.daniel <zsar.daniel@C19-TANAR>
2025-02-24 21:09:55 +0100

    Kevesebb komment

--- 2_14fi_WPF_masodik/ServerConnection.cs
+++ 2_14fi_WPF_masodik/ServerConnection.cs
@@ -88,22 +88,15 @@ namespace _2_14fi_WPF_masodik
                 {
                     nyerte = result
                 };
-                //JSON.stringify(jsonData)
                 string stringifiedJson = JsonConvert.SerializeObject(jsonData);
                 System.Windows.MessageBox.Show(stringifiedJson);
-                //Itt adjuk meg mit küldünk és mi lesz a headerben
                 StringContent sendThis = new StringContent(stringifiedJson, Encoding.UTF8, "Application/JSON");
                 //kell az authorization header: (setRequestHeader)
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", data.token);
-                //Ő a send és a ReadyState == XMLHttpRequest.DONE
                 HttpResponseMessage response = await client.PostAsync(url, sendThis);
-                //Megnézi, hogy a status code nem 4-el vagy 5-el kezdődik
                 response.EnsureSuccessStatusCode();
-                //kiolvassuk a responsból hogy mit küldött a szerver
                 string responseText = await response.Content.ReadAsStringAsync();
-                //Átalakítjuk a kapott string információt a megfelelő típusú objektumra, ami egyezik a küldött json objektummal
                 JsonResponse responseJson = JsonConvert.DeserializeObject<JsonResponse>(responseText);
-                //A szerver üzenetének kiírása
                 System.Windows.MessageBox.Show(responseJson.token, responseJson.message);
             }
             catch (Exception e)
